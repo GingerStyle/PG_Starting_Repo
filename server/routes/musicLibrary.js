fc4702753faca82c93005e479e7de0e1c6c38eb4
Myles Voigt <myles.voigt@gmail.com>
2022-12-13 21:02:14 -0600

    updated notes

--- server/routes/musicLibrary.js
+++ server/routes/musicLibrary.js
@@ -1,25 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-
-const Pool = pg.Pool;
-
-const pool = new Pool({
-    database: 'lydian_intro',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000,
-});
-
-pool.on('connect', () => {
-    console.log('postgres is connected');
-});
-
-// pool.on error
-pool.on('error', (error) => {
-    console.log('there was an error', error);
-});
+const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
     let queryText = 'SELECT * from songs;';
@@ -34,12 +15,37 @@ router.get('/', (req, res) => {
     //res.send(dbResponse);
 });
 
+router.get('/:id', (req ,res) =>{
+    console.log('hello from get request', req.params.id);
+    const queryText = `SELECT * FROM songs WHERE id = ${req.params.id};`;
+    pool.query(queryText)
+    .then((result) => {
+        console.log('results from db', result);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('error making a query', error);
+        res.sendStatus(500);
+    });
+});
+
+router.delete('/:id', (req ,res) =>{
+    console.log('hello from delete request', req.params.id);
+    const queryText = `DELETE FROM songs WHERE id = ${req.params.id};`;
+    pool.query(queryText)
+    .then((result) => {
+        console.log(result);
+        res.send(204);
+    }).catch((error) => {
+        console.log('error making a query', error);
+        res.sendStatus(500);
+    });
+});
+
 router.post('/', (req, res) => {
     const newSong = req.body;
     //quotes around string values, no quotes around integer values
-    const queryText = `INSERT INTO "songs" ("rank", "artist", "track", "published")
-                        values (${newSong.rank}, '${newSong.artist}', '${newSong.track}', '${newSong.published}');`;
-    pool.query(queryText)
+    const queryText = `INSERT INTO "songs" ("rank", "artist", "track", "published") VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [newSong.rank, newSong.artist, newSong.track, newSong.published])
     .then((result) => {
         console.log('result', result);
         res.sendStatus(201);
@@ -48,6 +54,8 @@ router.post('/', (req, res) => {
         console.log('error making insert query', error);
         res.sendStatus(500);
     });
-})
+});
+
+
 
 module.exports = router;
